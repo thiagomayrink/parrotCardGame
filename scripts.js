7823442ab8ddf1850efbf9fc299858c1e196666b
Thiago Paiva <thiagoscmpaiva@gmail.com>
2021-04-09 19:12:13 -0300

    refatorados espacamentos + identacao + nomes de variaveis

--- scripts.js
+++ scripts.js
@@ -1,32 +1,28 @@
 // variaveis globais
-let qtd_cartas = parseInt(prompt("Com quantas cartas você quer jogar? (apenas números pares entre 4 e 14)"));
+let qtdCartas = parseInt(prompt("Com quantas cartas você quer jogar? (apenas números pares entre 4 e 14)"));
 let contadorCertas = 0;
 let contadorJogadas = 0;
 const foramViradas =[];
 const todasAsCartas = ['img/fiestaparrot.gif', 'img/fiestaparrot.gif','img/bobrossparrot.gif', 'img/bobrossparrot.gif', 'img/explodyparrot.gif', 'img/explodyparrot.gif', 'img/metalparrot.gif', 'img/metalparrot.gif', 'img/revertitparrot.gif', 'img/revertitparrot.gif', 'img/tripletsparrot.gif', 'img/tripletsparrot.gif', 'img/unicornparrot.gif', 'img/unicornparrot.gif']
-//travar jogadas.
+//interromper jogadas.
 let bloquear = false;
-
 function iniciarJogo() {
     //escolher o numero de cartas
-    while (qtd_cartas !== 4 && qtd_cartas !== 6 && qtd_cartas !== 8 && qtd_cartas !== 10 && qtd_cartas !== 12 && qtd_cartas !== 14) {
-        qtd_cartas = parseInt(prompt("Com quantas cartas você quer jogar? (apenas números pares entre 4 e 14)"));
+    while (qtdCartas !== 4 && qtdCartas !== 6 && qtdCartas !== 8 && qtdCartas !== 10 && qtdCartas !== 12 && qtdCartas !== 14) {
+        qtdCartas = parseInt(prompt("Com quantas cartas você quer jogar? (apenas números pares entre 4 e 14)"));
     }
-
     //embaralha cartas
     function comparador() { 
         return Math.random() - 0.5; 
     }
-
     //embaralha cartas e define quantidade.
     let cartasEmbaralhadas = [];
     cartasEmbaralhadas = todasAsCartas;
-    cartasEmbaralhadas.length = qtd_cartas;
+    cartasEmbaralhadas.length = qtdCartas;
     cartasEmbaralhadas = cartasEmbaralhadas.sort(comparador);
-    
     //dar as cartas
-    for (let i =0; i < qtd_cartas; i++) {
-        let mesa = document.querySelector('.conteudo')
+    for (let i =0; i < qtdCartas; i++) {
+        let mesa = document.querySelector('.conteudo');
         mesa.innerHTML += `        
         <div class="carta" onclick="virar(this)">
             <div class="front-face face">
@@ -39,7 +35,6 @@ function iniciarJogo() {
     `;
     }
 }
-
 function virar(virada) {
     //a carta está virada?
     const foiVirada = virada.classList.contains('ativa');
@@ -47,7 +42,6 @@ function virar(virada) {
     if (bloquear){
         return;
     }
-
     if(foiVirada === false) {
         virada.classList.add('ativa');
         foramViradas.push(virada);
@@ -65,7 +59,6 @@ function virar(virada) {
             contadorCertas +=2;
         } 
         // caso não possuam o mesmo conteudo, vire as duas cartas para baixo.
-        //else if (foramViradas[0].innerHTML !== foramViradas[1].innerHTML)
         else {
             function desvirar() {
                     cartasAtivas[0].classList.remove('ativa');
@@ -74,17 +67,16 @@ function virar(virada) {
                     bloquear = false;
                 }
             }
+            //impede que uma terceira carta seja virada antes de desvirar as duas anteriores.
             bloquear = true;
             setTimeout(desvirar,1000)
         }
         foramViradas.length = 0;
     }
     contadorJogadas++
-    if (contadorCertas === qtd_cartas){
+    if (contadorCertas === qtdCartas){
         setTimeout(alert,1000,`Você ganhou em ${contadorJogadas} jogadas!`)
-        //alert(`Você ganhou em ${contadorJogadas} jogadas!`);
     }
 }
-
 //inicia o jogo ao carregar a página.
 window.onload = iniciarJogo;
